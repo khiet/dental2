d1bc6572083c743e2b83cc1cff099a30640ad08e
Khiet Le <khiet@outlook.com>
2024-03-12 21:04:24 +0000

    Fix migrations

--- db/migrate/20140209160816_add_weight_to_worker.rb
+++ db/migrate/20140209160816_add_weight_to_worker.rb
@@ -1,10 +1,6 @@
 class AddWeightToWorker < ActiveRecord::Migration[7.0]
   def up
     add_column :workers, :weight, :integer, default: 0
-
-    Worker.all.each_with_index do |worker, index|
-      worker.update_column(:weight, index)
-    end
   end
 
   def down
