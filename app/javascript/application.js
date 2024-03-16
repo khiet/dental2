af992dcc0398123ba091d6222790a849f524570d
Khiet Le <khiet@outlook.com>
2024-03-16 17:33:14 +0000

    Add cookie privacy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,4 +17,24 @@ document.addEventListener('DOMContentLoaded', () => {
       e.currentTarget.classList.add('left-menu-focused');
     });
   });
+
+  // cookie privacy
+  /* https://www.w3schools.com/js/js_cookies.asp */
+  var setCookie = function(cname, cvalue, exdays, cpath) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=" + cpath;
+  };
+
+  const jsSetCookiePrivacy = document.querySelector('.js-set-cookie-privacy');
+  if (jsSetCookiePrivacy) {
+    jsSetCookiePrivacy.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      setCookie('EU_COOKIE_LAW_CONSENT', 'yes', 3650, '/');
+
+      document.querySelector('.js-cookie-privacy').classList.add('hidden');
+    });
+  }
 });
